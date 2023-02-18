3eb0072799ec352b07e879e0f05d5fc56debc84d
alliajagbe <alliajagbe001@gmail.com>
2023-02-18 22:57:45 +0530

    longestPrefix

--- leetcodes.py
+++ leetcodes.py
@@ -1,23 +1,22 @@
 #%%
 
 def longestCommonPrefix(strs) -> str:
-    shortest_word = min(strs, key=len)
-    print("shortest word",shortest_word)
-    i = 1
-    while i <= len(shortest_word):
-        for element in strs:
-            # temp = shortest_word[:i]
-            if shortest_word[:i] == element[:i]:
-                continue
-            else:
-                return ""
-        
-        i += 1
-    final_prefix = shortest_word[:i]
-    print(final_prefix)
+    if len(strs) == 0:
+        return ""
+    elif len(strs) == 1:
+        return strs[0]
+    else:
+        shortest_word = min(strs, key=len)
+        print("shortest word",shortest_word)
+        i = 1
+        while i <= len(shortest_word):
+            for element in strs:
+                # temp = shortest_word[:i]
+                if shortest_word[:i] != element[:i]:
+                    return shortest_word[:i]
+            
+            i += 1
 
-    return final_prefix
-
-longestCommonPrefix(["flower","flow","flight"])
+print(longestCommonPrefix(["ab","a"]))
 
 #%%
