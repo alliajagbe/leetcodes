95444aa7627ea97df4d406ac3d4e8d3728b69e36
alliajagbe <alliajagbe001@gmail.com>
2023-02-18 23:46:40 +0530

    binary search

--- leetcodes.py
+++ leetcodes.py
@@ -19,3 +19,21 @@ def longestCommonPrefix(strs) -> str:
 print(longestCommonPrefix(["ab","a"]))
 
 # %%
+
+class Solution:
+    def search(nums, target: int) -> int:
+        high = len(nums) - 1
+        low = 0
+        while low <= high:
+            mid = (high+low)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                return -1
+
+print(Solution.search([-1,0,3,5,9,12], 9))
+# %%
