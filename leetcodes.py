48afc361a1d78de1573b4a3e5bd8488b943d2402
alliajagbe <alliajagbe001@gmail.com>
2023-02-23 17:13:01 +0530

    twoSums

--- leetcodes.py
+++ leetcodes.py
@@ -38,22 +38,21 @@ print(Solution.search([-1,0,3,5,9,12], 9))
 # %%
 
 # Two Sums
-'''
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-'''
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
 
 class Solution:
     def twoSum(nums, target: int):
-        print(sum(nums))
-        for i,j in zip(nums,nums):
-            if (i + j == target):
-                return [nums.index(i),nums.index(j)]
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i]+nums[j]) == target:
+                    return [i,j]
+                else:
+                    continue
 
-print(Solution.twoSum([2,7,11,15],9))
+print(Solution.twoSum([3,3],6))
         
 
 # %%
