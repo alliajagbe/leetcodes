135fd0009b263da93ee010fe8aae5087aa3219bb
alliajagbe <alliajagbe001@gmail.com>
2023-02-24 01:13:25 +0530

    threeSums

--- leetcodes.py
+++ leetcodes.py
@@ -83,7 +83,7 @@ class Solution:
             for i in range(len(nums)):
                 for j in range(i+1, len(nums)):
                     for k in range(j+1, len(nums)):
-                        if (nums[i] + nums[j] + nums[k] == 0):
+                        if (nums[i] + nums[j] + nums[k] == 0) and i!=j and j!=k and i!=k:
                             answer.append([nums[i], nums[j], nums[k]])
             n += 1
         
