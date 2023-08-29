0b30d868143b579105b58d8a64e0f5ffd3649b04
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 17:16:47 +0530

    kidswithcandies question

--- leetcodes.py
+++ leetcodes.py
@@ -145,8 +145,20 @@ class Solution:
     
 print(Solution.gcdOfStrings("ABCABC", "ABC"))
 # %%
+'''
+There are n kids with candies. 
+You are given an integer array candies, 
+where each candies[i] represents the number of candies the ith kid has, 
+and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, 
+where result[i] is true if, after giving the ith kid all the extraCandies, 
+they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+'''
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+    def kidsWithCandies(candies, extraCandies: int) -> List[bool]:
         # we define a function to give the maximum number of candies a kid has in the list
         def maximum(candies):
             max_candy = 0
