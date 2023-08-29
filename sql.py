52205c0e42c2b753423ed26ceaf36c9401586ef0
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 23:53:30 +0530

    find customer referee

--- sql.py
+++ sql.py
@@ -12,3 +12,10 @@ sql = """select product_id
             and recyclable = 'Y'
         """
 
+#%%
+'''
+# Find Customer Referee
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+'''
