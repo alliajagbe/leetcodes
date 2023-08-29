4c94a9a7b01ed57d84ade1398aab1ac6d7ccdcfb
alliajagbe <alliajagbe001@gmail.com>
2023-08-30 00:11:53 +0530

    sql

--- sql.py
+++ sql.py
@@ -45,3 +45,10 @@ Write a solution to find the name, population, and area of the big countries.
 
 Return the result table in any order.
 '''
+
+sql = """
+        select name, population, area 
+        from World 
+        where area >= 3000000 
+            or population >= 25000000
+            """
