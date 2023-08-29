3b9e1f1a0ba39806b0bef1989d021ed56739d732
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 23:39:31 +0530

    second iteration

--- leetcodes.py
+++ leetcodes.py
@@ -276,10 +276,10 @@ class Solution:
         suffix = [1] * len(nums)
 
         for i in range(1, len(nums)):
-            prefix[i] *= nums[i]
+            prefix[i-1] *= nums[i]
 
         for i in range(len(nums)-1, -1, -1):
-            suffix[i] *= nums[i]
+            suffix[i-1] *= nums[i]
 
         new_list = [prefix[i]*suffix[i] for i in range(len(nums))]
         return new_list
