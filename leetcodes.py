8f9c5e7178de9a2e701cc913742c3c5bba435a93
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 12:58:53 +0530

    merge alternatively

--- leetcodes.py
+++ leetcodes.py
@@ -92,3 +92,17 @@ class Solution:
 
 print(Solution.threeSum([-1,0,1,2,-1,-4]))
 # %%
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        new_string = ''
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                new_string += word1[i]
+            if i < len(word2):
+                new_string += word2[i]
+
+            i += 1
+
+        return new_string
