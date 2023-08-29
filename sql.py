cfa895f4f09480c146283136c5d8f4b2b9cabe71
alliajagbe <alliajagbe001@gmail.com>
2023-08-30 00:13:50 +0530

    article views i

--- sql.py
+++ sql.py
@@ -52,3 +52,11 @@ sql = """
         where area >= 3000000 
             or population >= 25000000
             """
+
+#%%
+'''
+# Article Views I
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+'''
