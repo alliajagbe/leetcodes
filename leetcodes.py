023e1a8306ddee0d237ebd20ed3a210a6440e219
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 17:23:19 +0530

    gcdofstrings question

--- leetcodes.py
+++ leetcodes.py
@@ -125,6 +125,14 @@ class Solution:
 print(Solution.mergeAlternately("ab", "pqrs"))
 print(Solution.mergeAlternately2("ab", "pqrs"))
 # %%
+'''
+For two strings s and t, 
+we say "t divides s" if and only if s = t + ... + t 
+(i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, 
+return the largest string x such that x divides both str1 and str2.
+'''
 class Solution:
     def gcdOfStrings(str1: str, str2: str) -> str:
 
