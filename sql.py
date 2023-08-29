708a0a668840973e3cf5bd2278624bea228b701a
alliajagbe <alliajagbe001@gmail.com>
2023-08-30 00:21:39 +0530

    invalid tweets prompt

--- sql.py
+++ sql.py
@@ -66,3 +66,13 @@ sql = """
         where author_id = viewer_id
         order by author_id
         """
+
+#%%
+'''
+# Invalid Tweets
+Write a solution to find the IDs of the invalid tweets. 
+The tweet is invalid if the number of characters used in the content of 
+the tweet is strictly greater than 15.
+
+Return the result table in any order.
+'''
