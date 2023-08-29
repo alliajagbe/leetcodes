95c05afd883630996cd17eee304212fa73d06100
alliajagbe <alliajagbe001@gmail.com>
2023-08-29 17:11:28 +0530

    canPlaceFlowers test

--- leetcodes.py
+++ leetcodes.py
@@ -162,3 +162,18 @@ class Solution:
         # in the list
         boolean_array = [candy + extraCandies >= maximum(candies) for candy in candies]
         return boolean_array
+
+#%%
+class Solution:
+    def canPlaceFlowers(flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i==0 or flowerbed[i-1]==0) and (i==len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+
+            if n == 0:
+                return True
+        
+        return False
