76709e752720c5845a508f1d3018b032d00fb02f
alliajagbe <alliajagbe001@gmail.com>
2023-09-08 23:40:00 +0530

    duplicates function

--- data-science-prep.py
+++ data-science-prep.py
@@ -14,3 +14,5 @@ intersection([1,5,9,0],[3,0,2,9])
 Given an array, find all the duplicates in the array.
 For example, input: [1,2,3,1,3,6,5], output: [1,3]
 '''
+def duplicates(x):
+    return [i for i in x if x.count(i) > 1]
