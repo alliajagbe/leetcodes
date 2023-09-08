4cb8e3d81083d4c28c1ab949d172e994e8ca8502
alliajagbe <alliajagbe001@gmail.com>
2023-09-08 23:13:07 +0530

    is Subsequence question

--- leetcodes.py
+++ leetcodes.py
@@ -414,3 +414,15 @@ class Solution:
         return nums
 
 print(Solution.moveZeroes([0,1,0,3,12]))
+
+# %%
+'''
+# Is Subsequence
+Given two strings s and t, return true if s is a subsequence of t, 
+or false otherwise.
+
+A subsequence of a string is a new string that is formed from the 
+original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. 
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+'''
