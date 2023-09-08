e9666ea20ece6be1544b30a4a1a010fcd672c8d7
alliajagbe <alliajagbe001@gmail.com>
2023-09-08 23:38:44 +0530

    duplicates function

--- data-science-prep.py
+++ data-science-prep.py
@@ -9,3 +9,8 @@ def intersection(x,y):
 # function test
 intersection([1,5,9,0],[3,0,2,9])
 # %%
+
+'''
+Given an array, find all the duplicates in the array.
+For example, input: [1,2,3,1,3,6,5], output: [1,3]
+'''
