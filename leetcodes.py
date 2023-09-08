fe01a86e7fb73c950c359a27289d8b9f5f9d3c3f
alliajagbe <alliajagbe001@gmail.com>
2023-09-08 23:09:58 +0530

    updated movezeroes function

--- leetcodes.py
+++ leetcodes.py
@@ -398,7 +398,7 @@ maintaining the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 '''
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes(nums) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
