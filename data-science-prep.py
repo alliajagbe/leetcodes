1fd31eb3b254023a91d72605958c9e331e00c544
alliajagbe <alliajagbe001@gmail.com>
2023-09-08 23:35:54 +0530

    intersection function code

--- data-science-prep.py
+++ data-science-prep.py
@@ -3,3 +3,5 @@
 Given two arrays, write a python function to return the intersection of the two. 
 For example, X = [1,5,9,0] and Y = [3,0,2,9], it should return [9,0].
 '''
+def intersection(x,y):
+    return set(x).intersection(set(y))
