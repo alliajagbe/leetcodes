1f20f8c1ea6c3f4c3ebf8cd1b9beefc4f50bc63f
alliajagbe <alliajagbe001@gmail.com>
2023-09-09 23:35:02 +0530

    corrected maxproduct function

--- data-science-prep.py
+++ data-science-prep.py
@@ -28,8 +28,8 @@ three numbers in the array.
 def maxProduct(x):
     # getting the first three maximum positive numbers
     positives = [i for i in x if i > 0]
-    first_three = sorted(positives)[:3]
-    return first_three[-1]*first_three[-2]*first_three[-3]
+    first_three = sorted(positives)[-3:]
+    return first_three[0]*first_three[1]*first_three[2]
 
 maxProduct([2,6,3,1,5,7,8])
 
